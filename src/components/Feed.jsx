9e5fbc614dabfcb135a1e0ec0b93656e2b12c8b3
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-11-16 15:19:12 -0500

    upload modal ready to go

---
+++ src/components/Feed.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+import { Button } from "antd"
+import Upload from "./Upload"
+
+export default function Feed() {
+    const [photoList, setPhotoList] = useState()
+    const [showUpload, setShowUpload] = useState(false)
+    useEffect(() => {
+        fetch('https://express-ts-gk.web.app/photos')
+        .then(results => results.json())
+        .then(data => setPhotoList(data))
+        .catch(alert)
+    }, [setPhotoList])
+    return (
+        <section>
+            {!photoList
+            ? <p>Loading...</p>
+            : <p>{photoList.length}</p>
+            
+            }
+            {showUpload ? <Upload /> : null}
+            <Button onClick={() => setShowUpload(true)}
+            className="fab" 
+            type="primary" 
+            shape="circle" 
+            size="">+</Button>
+        </section>
+        
+    )
+
+}
---
+++ src/components/Upload.jsx
@@ -0,0 +1,10 @@
+import { Modal } from "antd"
+
+
+export default function Upload() {
+    return (
+        <Modal title="Upload Photo" open={true}>
+            <p>Upload form goes here...</p>
+        </Modal>
+    )
+}
